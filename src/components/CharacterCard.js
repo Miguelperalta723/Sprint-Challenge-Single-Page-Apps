4c45545385cd8e1b0b84d8040c8f847354029335
Miguel Peralta <eudysperalta723@gmail.com>
2019-07-26 13:14:40 -0400

    Mvp completed

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
 import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard (props) {
+  return (
+  <Card>
+    <Image src={props.image} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{props.name}</Card.Header>
+      <Card.Meta>Status: {props.status}</Card.Meta>
+      <Card.Description>
+        Species: {props.species}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='tv' />
+        Appears in {props.episodeCount.length} episodes
+      </a>
+    </Card.Content>
+  </Card>
+  )
 }
+
+  
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Card } from 'semantic-ui-react'
 
 export default function LocationCard ({ name, type, dimension, residents }) {
-  // image={image}
-  return (<span>todo: location</span>
+  const allLocations = [
+  {
+    header: `${name}`,
+    description: `Dimension: ${dimension}`,
+    meta: `Type: ${type}`,
+  }
+]
+  return (
+    <Card.Group items={allLocations} />
   )
 }
